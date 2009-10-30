8a152def8a18fa53b982d9850446eac77c41545a
RayMorgan <ray@simple-apps.com>
2009-10-29 23:55:10 -0700

    Added a utility function to parse the body of the incoming request.
    Also added a demo for it.

---
+++ test/body-parsing.js
@@ -0,0 +1,38 @@
+var Gateway = require('../lib/gateway.js');
+var sys = require('/sys.js');
+
+function MyApp(request, response) {
+  if (request.requestMethod === 'POST') {
+    Gateway.utils.parseURLEncodedBody(request, function (body) {
+      sys.p(body);
+    });
+  }
+  
+  var contents = '\
+<html>            \
+  <body>          \
+    <form action="" method="post">                          \
+      Name: <input type="text" name="person[name]" /><br /> \
+      Age:  <input type="text" name="person[age]" /><br />  \
+      <input type="submit" value="Go" />                    \
+    </form>       \
+  </body>         \
+</html>           \
+  ';
+  
+  response.sendHeader(200, {
+    'content-type': 'text/html',
+    'content-length': contents.length
+  });
+  response.sendBody(contents);
+  response.finish();
+}
+
+
+var builder = Gateway.createBuilder();
+
+builder.use(Gateway.Middleware.CommonLogger);
+builder.use(Gateway.Middleware.ContentType);
+builder.use(MyApp);
+
+builder.boot(Gateway.Handler.NodeHttp, {port: 8000});
